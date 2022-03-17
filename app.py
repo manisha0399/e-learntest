168ccb4a875fd7c1d9988bbe5c6183efd7d32ae9
manisha shedge <ms3440292@gmail.com>
2022-03-17 13:07:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,203 @@
+
+from flask import Flask, render_template, request,session,redirect,flash,url_for,send_file
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import os
+from flask import Flask,  request
+import re
+
+
+app = Flask(__name__,template_folder='templates')
+app.secret_key = 'super-secret-key'
+
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Register(db.Model):
+    __tablename__ = 'register'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    uname = db.Column(db.String(200))
+    email = db.Column(db.String(200),unique=True)
+    password =db.Column(db.String(10))
+    cpassword =db.Column(db.String(10))
+
+    def __init__(self, name, uname, email, password, cpassword):
+        self.name = name
+        self.uname = uname
+        self.email = email
+        self.password = password
+        self.cpassword = cpassword
+
+
+@app.route("/")
+def Home():
+    return render_template('index.html')
+
+@app.route("/about")
+def About():
+    return render_template('about.html')
+
+@app.route("/album")
+def Album():
+    return render_template('album.html')
+
+
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        uname = request.form['uname']
+        email = request.form['email']
+        password = request.form['password']
+        cpassword = request.form['cpassword']
+        
+        if name == '' or uname == '' or email == '' or password == '' or cpassword == '':
+            return render_template('register.html', message='Please enter required fields')
+        if db.session.query(Register).filter(Register.email == email).count() == 0:
+            data = Register(name, uname, email, password, cpassword)
+            db.session.add(data)
+            db.session.commit()
+           
+            return render_template('success.html')
+        return render_template('register.html', message='You have already submitted feedback')
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if (request.method== "GET"):
+        if('email' in session and session['email']):
+            return render_template('dashboard.html')
+        else:
+            return render_template("login.html")
+
+    if (request.method== "POST"):
+        email = request.form["email"]
+        password = request.form["password"]
+        
+        login = Register.query.filter_by(email=email, password=password).first()
+        if login is not None:
+            session['email']=email
+            return render_template('dashboard.html')
+        else:
+            flash("plz enter right password",'error')
+            return render_template('login.html')
+
+@app.route("/contact",  methods=['GET','POST'])
+def contact():
+    if(request.method =='POST'):
+        name=request.form.get('name')
+        email=request.form.get('email')
+        subject=request.form.get('subject')
+        message=request.form.get('message')
+        entry=Contact(name=name,email=email,subject=subject,message=message)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
+
+@app.route("/web",methods=['GET','POST'])
+def web_development():
+    return render_template('web.html')
+
+@app.route("/andriod",methods=['GET','POST'])
+def andriod_development():
+    return render_template('andriod.html')
+
+@app.route("/art",methods=['GET','POST'])
+def Art():
+    return render_template('art.html')
+
+@app.route("/photography",methods=['GET','POST'])
+def Photography():
+    return render_template('photography.html')
+
+@app.route("/uxdesign",methods=['GET','POST'])
+def Uxdesign():
+    return render_template('uxdesign.html')
+
+@app.route("/graphic",methods=['GET','POST'])
+def Graphic_design():
+    return render_template('graphic.html')
+
+@app.route("/marketing",methods=['GET','POST'])
+def Digital_marketing():
+    return render_template('marketing.html')
+
+@app.route("/linux",methods=['GET','POST'])
+def Linux_development():
+    return render_template('linux.html')
+
+@app.route("/web_development")
+def download_file():
+    p="website_development_tutorial.pdf"
+    return send_file(p,as_attachment=True)
+
+@app.route("/andriod_development")
+def andriod_download_file():
+    c="android_tutorial.pdf"
+    return send_file(c,as_attachment=True)
+
+@app.route("/art_craft")
+def art_download_file():
+    d="art_and_design-_a_comprehensive_guide_for_creative_artists.pdf"
+    return send_file(d,as_attachment=True)
+
+@app.route("/photo")
+def photo_download_file():
+    m="photography.pdf"
+    return send_file(m,as_attachment=True)
+
+@app.route("/ux")
+def ux_download_file():
+    k="ux design.pdf"
+    return send_file(k,as_attachment=True)
+
+@app.route("/graphics")
+def graphics_download_file():
+    n="Graphic Design.pdf"
+    return send_file(n,as_attachment=True)
+
+@app.route("/digital")
+def digital_download_file():
+    s="Digital Marketing.pdf"
+    return send_file(s,as_attachment=True)
+
+@app.route("/linux_development")
+def linux_download_file():
+    a="Linux development.pdf"
+    return send_file(a,as_attachment=True)
+
+@app.route("/adminlogin",methods=['GET','POST'])
+def adminlogin():
+    if(request.method=='POST'):
+        email=request.form.get('email')
+        password=request.form.get('password')
+        if email=='admin@gmail.com' and password=='123456':
+            return render_template('admindash.html') 
+    return render_template('adminlogin.html')
+
+
+
+@app.route("/logout", methods = ['GET','POST'])
+def logout():
+    session.pop('email')
+    return redirect(url_for('index.html')) 
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == '__main__':
+    app.run()
